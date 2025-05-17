c4964421a85412a866a7661f6691c78e131dd794
Teguh Ramadhan <teguhrmdhn@gmail.com>
2025-05-17 20:55:07 +0700

    add navbar navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        'primary': '#405D5B',
-        'soft-green': '#A7F3D0',
+        primary: "#405D5B",
+        "soft-green": "#A7F3D0",
       },
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        libreBodoni: ['Libre Bodoni', 'sans-serif'],
+        inter: ["Inter", "sans-serif"],
+        libreBodoni: ["Libre Bodoni", "sans-serif"],
+      },
+      animation: {
+        "fade-in": "fadeIn 0.8s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
       },
     },
   },
